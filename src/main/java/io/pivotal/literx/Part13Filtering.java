5988b828f253ef0593464126557a4cfc4508401a
Marcin Chrost <marcin.chrost@jcommerce.pl>
2018-09-12 15:52:23 +0200

    Part 13 - Filtering

---
+++ src/main/java/io/pivotal/literx/Part13Filtering.java
@@ -0,0 +1,39 @@
+package io.pivotal.literx;
+
+import io.pivotal.literx.domain.User;
+import reactor.core.publisher.Flux;
+
+/**
+ * Learn how to filter Flux elements
+ */
+public class Part13Filtering {
+
+//========================================================================================
+
+    // TODO Remove users whose names are equal to White
+    Flux<User> removeUsersWithWhiteName(Flux<User> flux) {
+        return null;
+    }
+
+//========================================================================================
+
+    // TODO Remove duplicated users
+    Flux<User> removeDuplicatedUsers(Flux<User> flux) {
+        return null;
+    }
+
+//========================================================================================
+
+    // TODO Remove users with duplicated name
+    Flux<User> removeUsersWithDuplicatedNames(Flux<User> flux) {
+        return null;
+    }
+
+//========================================================================================
+
+    // TODO Remove duplicated users in sequences
+    Flux<User> removeDuplicatedUsersInSequences(Flux<User> flux) {
+        return null;
+    }
+
+}
