91ae6372c683aaa935ffef087543a58d1dcf55ef
Marcin Chrost <marcin.chrost@jcommerce.pl>
2018-09-12 15:55:39 +0200

    Part 13 - Filtering (solution)

--- src/main/java/io/pivotal/literx/Part13Filtering.java
+++ src/main/java/io/pivotal/literx/Part13Filtering.java
@@ -12,28 +12,28 @@ public class Part13Filtering {
 
     // TODO Remove users whose names are equal to White
     Flux<User> removeUsersWithWhiteName(Flux<User> flux) {
-        return null;
+        return flux.filter(u -> !"White".equals(u.getLastname()));
     }
 
 //========================================================================================
 
     // TODO Remove duplicated users
     Flux<User> removeDuplicatedUsers(Flux<User> flux) {
-        return null;
+        return flux.distinct();
     }
 
 //========================================================================================
 
     // TODO Remove users with duplicated name
     Flux<User> removeUsersWithDuplicatedNames(Flux<User> flux) {
-        return null;
+        return flux.distinct(User::getLastname);
     }
 
 //========================================================================================
 
     // TODO Remove duplicated users in sequences
     Flux<User> removeDuplicatedUsersInSequences(Flux<User> flux) {
-        return null;
+        return flux.distinctUntilChanged();
     }
 
 }
