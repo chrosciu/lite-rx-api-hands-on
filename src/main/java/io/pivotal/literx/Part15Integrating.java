c210a5b098258f85470f37d3078fb738accee2f4
Marcin Chrost <marcin.chrost@jcommerce.pl>
2018-11-25 13:10:29 +0100

    Fixed description

--- src/main/java/io/pivotal/literx/Part15Integrating.java
+++ src/main/java/io/pivotal/literx/Part15Integrating.java
@@ -8,7 +8,7 @@ import reactor.core.publisher.Mono;
 public class Part15Integrating {
 
 //========================================================================================
-    // TODO Sum all points for all users (beware of traps !). Discard users whose sum of points is less than or equal to 5
+    // TODO Sum all points for all users (beware of traps !). Discard points which value is less than or equal to 5
     public Mono<Integer> sumUsersPoints(IterableRepository<User> iterableUserRepository, UserPointsService userPointsService) {
         return null;
     }
