514b18863ec03eff9d79c2060cbf0fdac47a20d6
Marcin Chrost <marcin.chrost@jcommerce.pl>
2018-12-31 14:35:01 +0100

    Part19 - Context - solution

--- src/main/java/io/pivotal/literx/Part19Context.java
+++ src/main/java/io/pivotal/literx/Part19Context.java
@@ -2,6 +2,8 @@ package io.pivotal.literx;
 
 import io.pivotal.literx.domain.User;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.util.context.Context;
 
 /**
  * Learn how to use context in Reactor operators
@@ -13,13 +15,14 @@ public class Part19Context {
     // TODO Return flux that logs each user forst name with prefix taken from given context key
     // If context does not contain such key, given default value should be used instead
     Flux<String> logUserFirstName(Flux<User> flux, String key, String defaultValue) {
-        return null;
+        return flux.flatMap(user -> Mono.subscriberContext()
+                .map(ctx -> ctx.getOrDefault(key, defaultValue) + " " + user.getFirstname()));
     }
 
 //========================================================================================
 
     // TODO Fill given flux with context containing given value under given key and return filled flux
     Flux<User> fillUsersWithContext(Flux<User> flux, String key, String value) {
-        return null;
+        return flux.subscriberContext(Context.of(key, value));
     }
 }
