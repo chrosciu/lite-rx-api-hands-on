cbca4e09ae4a2c513b87bbc0ec53f5af43724bdd
Marcin Chrost <marcin.chrost@jcommerce.pl>
2019-10-21 21:24:52 +0200

    Part 23 - Subscription - solutions

--- src/main/java/io/pivotal/literx/Part23Subscribe.java
+++ src/main/java/io/pivotal/literx/Part23Subscribe.java
@@ -1,6 +1,8 @@
 package io.pivotal.literx;
 
 import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
 
 public class Part23Subscribe {
 
@@ -9,7 +11,38 @@ public class Part23Subscribe {
     // TODO Write a subscriber that request item in batches of #{batchSize}
     // and cancels subscription if number of received items reaches #{maxAmount}
     Subscriber<Integer> createBatchIntSubscriber(int batchSize, int maxAmount) {
-        return null;
+        return new BatchIntSubscriber(batchSize, maxAmount);
+    }
+
+    private class BatchIntSubscriber extends BaseSubscriber<Integer> {
+        private final int batchSize;
+        private final int maxAmount;
+
+        private int received = 0;
+
+        private BatchIntSubscriber(int batchSize, int maxAmount) {
+            this.batchSize = batchSize;
+            this.maxAmount = maxAmount;
+            if (batchSize <= 0 || maxAmount <= 0) {
+                throw new IllegalArgumentException("batchSize and maxAmount must be greater than 0");
+            }
+        }
+
+        @Override
+        protected void hookOnSubscribe(Subscription subscription) {
+            request(batchSize);
+        }
+
+        @Override
+        protected void hookOnNext(Integer value) {
+            ++received;
+            if (received >= maxAmount) {
+                cancel();
+            }
+            if (received % batchSize == 0) {
+                request(batchSize);
+            }
+        }
     }
 
 }
