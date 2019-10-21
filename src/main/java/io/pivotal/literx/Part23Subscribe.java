47f153b7d17f8772d39cae8d9f4efdd10518c773
Marcin Chrost <marcin.chrost@jcommerce.pl>
2019-10-21 21:20:24 +0200

    Part 23 - Subscription

---
+++ src/main/java/io/pivotal/literx/Part23Subscribe.java
@@ -0,0 +1,15 @@
+package io.pivotal.literx;
+
+import org.reactivestreams.Subscriber;
+
+public class Part23Subscribe {
+
+//========================================================================================
+
+    // TODO Write a subscriber that request item in batches of #{batchSize}
+    // and cancels subscription if number of received items reaches #{maxAmount}
+    Subscriber<Integer> createBatchIntSubscriber(int batchSize, int maxAmount) {
+        return null;
+    }
+
+}
