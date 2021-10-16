754db42cc29e7ebe45b2ac6f329b5e758f2ff849
Marcin Chrost <marcinc@backbase.com>
2021-10-16 20:41:07 +0200

    Clarified Part04 tasks description

--- src/main/java/io/pivotal/literx/Part04Transform.java
+++ src/main/java/io/pivotal/literx/Part04Transform.java
@@ -13,14 +13,14 @@ public class Part04Transform {
 
 //========================================================================================
 
-	// TODO Capitalize the user username, firstname and lastname
+	// TODO Capitalize (with String#toUpperCase) the user username, firstname and lastname
 	Mono<User> capitalizeOne(Mono<User> mono) {
 		return null;
 	}
 
 //========================================================================================
 
-	// TODO Capitalize the users username, firstName and lastName
+	// TODO Capitalize (with String#toUpperCase) the users username, firstName and lastName
 	Flux<User> capitalizeMany(Flux<User> flux) {
 		return null;
 	}
