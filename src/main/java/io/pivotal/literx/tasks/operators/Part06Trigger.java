4684a3393de291599d565d2e47ae14d6f4673673
Marcin Chrost <mchrost82@gmail.com>
2022-10-17 23:16:58 +0200

    Trigger operators - additional task

--- src/main/java/io/pivotal/literx/tasks/operators/Part06Trigger.java
+++ src/main/java/io/pivotal/literx/tasks/operators/Part06Trigger.java
@@ -1,5 +1,6 @@
 package io.pivotal.literx.tasks.operators;
 
+import io.pivotal.literx.domain.User;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -23,6 +24,13 @@ public class Part06Trigger {
 
     //TODO Return Mono<Void> which completes when all given streams complete
     Mono<Void> triggerCompleteWhenAllStreamsComplete(Flux<?>... streams) {
+        return null;
+    }
+
+    //========================================================================================
+
+    //TODO Return alternate Flux containing SKYLER and JESSE users if given stream is empty
+    Flux<User> returnFluxWithSkylerAndJesseWhenStreamIsEmpty(Flux<User> flux) {
         return null;
     }
 
