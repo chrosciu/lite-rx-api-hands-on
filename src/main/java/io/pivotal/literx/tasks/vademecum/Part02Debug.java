7d493085f151bead054bad5e1e5f1f1d43c75d86
Marcin Chrost <mchrost82@gmail.com>
2025-10-26 21:18:20 +0100

    Prefer doFirst() instead of doOnSubscribe()

--- src/main/java/io/pivotal/literx/tasks/vademecum/Part02Debug.java
+++ src/main/java/io/pivotal/literx/tasks/vademecum/Part02Debug.java
@@ -18,7 +18,7 @@ public class Part02Debug {
 
 //========================================================================================
 
-    // TODO Return a Flux with all users stored in the repository that prints "Starring:" on subscribe, "firstname lastname" for all values and "The end!" on complete
+    // TODO Return a Flux with all users stored in the repository that prints "Starring:" at first, "firstname lastname" for all values and "The end!" on complete
     Flux<User> fluxWithDoOnPrintln() {
         return null;
     }
